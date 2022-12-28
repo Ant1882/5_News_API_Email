247cfe299aaabcea9ad9b99de455018badd1195f
Ant1882 <antmarshall1882@gmail.com>
2022-12-28 14:45:23 +0000

    Specify language, limit, added subject

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "5a2f6ad2884b4b15b6142145ad61d28a"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2022-11-27&sortBy=publishedAt&apiKey=" \
-      "5a2f6ad2884b4b15b6142145ad61d28a"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      "apiKey=5a2f6ad2884b4b15b6142145ad61d28a&" \
+      "language=en"
 
 # Make request
 request = requests.get(url)
@@ -14,9 +18,11 @@ content = request.json()
 
 # Access article titles & url
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["url"] + 2*"\n"
+        body = "Subject: Today's news..." + "\n" + body \
+                + article["title"] + "\n" \
+                + article["url"] + 2*"\n"
         
 body = body.encode("utf-8")
 send_email(message=body)
